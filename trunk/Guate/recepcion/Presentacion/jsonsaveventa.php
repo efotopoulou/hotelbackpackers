5e7c1ecffd4b119c60e402ecfad209a877b495a2
fwtopoulou <fwtopoulou@776907b2-8993-11dd-9899-c7ba65f7c4c7>
2008-10-03 23:45:33 +0000

    ultimos cambios para venta recepcion y tpv recepcion comida

--- trunk/Guate/recepcion/Presentacion/jsonsaveventa.php
+++ trunk/Guate/recepcion/Presentacion/jsonsaveventa.php
@@ -2,28 +2,28 @@
 require ($_SERVER['DOCUMENT_ROOT'] . '/recepcion/Dominio/class_comanda.php');
 require ($_SERVER['DOCUMENT_ROOT'] . '/recepcion/Dominio/MensajeJSON.php');
 //Recoge el parametro y se limpia de contrabarras
-  $json = $_POST['json'];
- $mesaNum = $_POST['mesa'];
+ $json = $_POST['json'];
  $json = str_replace("\\", "",$json);
 
 //Creacion del objeto que inserta en la BD
 $comanda = new Comanda();
 $mensaje = new MensajeJSON();
  
-$mesa = json_decode($json, true);
-
-$mesa["numRow"];
+$comandaJson = json_decode($json, true);
 //idComanda,estado,fechaHora,usuario,efectivo,mesa,tipoCliente,Total
 try {
- $comanda->setComandaAbierta($mesa["comandaID"],$mesa["efectivo"],$mesaNum,$mesa["currentClientType"],$mesa["total"],$mesa["id_cliente"],$mesa["free"]);
+ $idComanda = $comanda->setComandaVenta($comandaJson["efectivo"],$comandaJson["currentClientType"],$comandaJson["total"],$comandaJson["id_cliente"],$comandaJson["free"]);
  //Se borra por si acaso ha desactivado el efectivo y lo vuelve a apretar.
  //$comanda->borrarLineasComanda($mesa["comandaID"]);
- $lineas = $mesa["liniasComanda"];
- for ($i=0;$i<=$mesa["numRow"];$i++){
+ $lineas = $comandaJson["liniasComanda"];
+ 
+ for ($i=0;$i<=$comandaJson["numRow"];$i++){
  	$cantidad = (int)$lineas[$i]["cantidad"];
  	if($cantidad==0) $cantidad=1;
- 	$comanda->setLineaComanda($mesa["comandaID"],$lineas[$i]["platoId"],$cantidad, $lineas[$i]["precioN"]);
+ 	$comanda->setLineaComanda($idComanda,$lineas[$i]["platoId"],$cantidad, $lineas[$i]["precioN"]);
  }
+ 
+//$comanda->setComandaCredito($idComanda); 
 }catch (SQLException $e){
 	$aux = $e ->getNativeError();
  $mensaje->setMensaje("Error Desconocido: $aux!");
