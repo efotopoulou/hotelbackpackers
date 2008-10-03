905b0bbbbe0c58f321744467b5bfc34ad5ccdbfb
fwtopoulou <fwtopoulou@776907b2-8993-11dd-9899-c7ba65f7c4c7>
2008-10-03 03:04:17 +0000

    problema al login. colores en venta en recepcion

--- Guate/recepcion/view.php
+++ Guate/recepcion/view.php
@@ -3,16 +3,6 @@ include($_SERVER['DOCUMENT_ROOT'] . '/common/Dominio/class_session.php');
 require_once ($_SERVER['DOCUMENT_ROOT'] . '/common/Dominio/class_log.php');
 ini_set('display_errors','1');
 
-function gestor_excepciones($excepcion) {
-  echo "Excepción no capturada: " , $excepcion->getMessage(), "\n";
-error_log($excepcion->getMessage()."\r\n", 3, $_SERVER['DOCUMENT_ROOT'] . '/hotel/log/error.log');
-}
-
-set_exception_handler('gestor_excepciones');
-
-
-
-
 ob_start();
 $sesion = new session();
 ob_end_clean();
@@ -22,11 +12,12 @@ $page='';
 if($_GET!=null){
 	$p_req=$_GET['page'];
 	
-	if($sesion->is_allowed_rest($p_req))
+	if($sesion->is_allowed_rest($p_req)){
 		$page='Presentacion/'.$p_req.'.php';
-	else
-		$page='/hotel/view.php?page=login'; 	
+		include($page);
+	}else
+		header('Location:/hotel/view.php'); 	
 }
 
-include($page);
+
 ?>
