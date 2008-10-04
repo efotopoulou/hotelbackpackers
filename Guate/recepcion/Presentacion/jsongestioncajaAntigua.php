27948b8b201ecf371f181da785236435441f7673
fwtopoulou <fwtopoulou@776907b2-8993-11dd-9899-c7ba65f7c4c7>
2008-10-04 21:52:58 +0000

    buton abrir la caja

--- Guate/recepcion/Presentacion/jsongestioncajaAntigua.php
+++ Guate/recepcion/Presentacion/jsongestioncajaAntigua.php
@@ -73,7 +73,7 @@ function ld_tikets_old($id_caja,$caja){
 $tikets = $caja->ld_tickets_old($id_caja);	
    if ((sizeof($tikets))>0){
 	  for($i=0;$i<count($tikets);$i++) {
-	  $TicketsInfo[$i]=array("idComanda"=>$tikets[$i]->idComanda,"fechaHora"=>$tikets[$i]->fechaHora,"total"=>$tikets[$i]->total,"efectivo"=>$tikets[$i]->efectivo,"tipoCliente"=>$tikets[$i]->tipoCliente,"nombre"=>$tikets[$i]->nombre,"free"=>$tikets[$i]->free);
+	  $TicketsInfo[$i]=array("idComanda"=>$tikets[$i]->idComanda,"numComanda"=>$tikets[$i]->numComanda,"estado"=>$tikets[$i]->estado,"fechaHora"=>$tikets[$i]->fechaHora,"total"=>$tikets[$i]->total,"efectivo"=>$tikets[$i]->efectivo,"tipoCliente"=>$tikets[$i]->tipoCliente,"nombre"=>$tikets[$i]->nombre,"free"=>$tikets[$i]->free);
 	  }
 	  return ($TicketsInfo);
    }		
