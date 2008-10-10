2fb042e16bc484d2979e0af7210949172e1da468
fwtopoulou <fwtopoulou@776907b2-8993-11dd-9899-c7ba65f7c4c7>
2008-10-10 00:17:40 +0000

    Control de stock con los tipos de Cliente

--- trunk/Guate/recepcion/Dominio/ventaturno.php
+++ trunk/Guate/recepcion/Dominio/ventaturno.php
@@ -3,11 +3,13 @@ class VentaTurno{
 	var $numBebida;
 	var $nombre;
 	var $suma;
+	var $clientType;
 	
-	function VentaTurno($numBebida,$nombre,$suma){
+	function VentaTurno($numBebida,$nombre,$suma,$clientType){
 	$this->numBebida = $numBebida;
 	$this->nombre = $nombre;
 	$this->suma = $suma;
+	$this->clientType = $clientType;
 	}
 }
 ?>
