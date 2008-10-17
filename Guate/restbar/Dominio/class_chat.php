61d562f801cd6c7b85efb9103f89e2104a762289
fwtopoulou <fwtopoulou@776907b2-8993-11dd-9899-c7ba65f7c4c7>
2008-10-17 22:00:11 +0000

    Chat acabado

--- Guate/restbar/Dominio/class_chat.php
+++ Guate/restbar/Dominio/class_chat.php
@@ -3,21 +3,27 @@ require_once ('ComunicationRestBar.php');
 
 class chat{
  
-	const LEERCHAT= 'select pseudo, msg, id_msg from (select pseudo, msg, id_msg from chat_message where pseudo!=? and id_msg>? order by id_msg desc limit 50) as messages order by messages.id_msg';
-	const ESCRIBIRCHAT= 'INSERT INTO chat_message VALUES (?,?, 0)';
+	const LEERCHAT= 'select pseudo as nickname, msg, time, id_msg from (select pseudo, msg, time,id_msg from chat_message where pseudo!=? and id_msg>? order by id_msg desc limit 50) as messages order by messages.id_msg';
+	const ESCRIBIRCHAT= 'INSERT INTO chat_message VALUES (?,?, 0, ?)';
 	
 		function leerChat($pseudo){
 		$comunication = new ComunicationRestBar();
 		$PARAMS = array($pseudo, $_SESSION['last_chat_message_id']);
 		$PARAMS_TYPES = array (ComunicationRestBar::$TSTRING, ComunicationRestBar::$TINT);
 		$result = $comunication->query(self::LEERCHAT,$PARAMS,$PARAMS_TYPES);
-		
-		return $result;
+	    if($result->getRecordCount()>0 ) {
+	    	$i=0;
+		    while($result->next()){
+				$resultc=$result->getRow();
+				$a[$i]=array("nickname"=>$resultc["nickname"], "msg"=>$resultc["msg"],"time"=>$resultc["time"],"id_msg"=>$resultc["id_msg"]);
+		    }
+		 }		
+		return $a;
 		}
-		function escribirChat($pseudo, $msg){
+		function escribirChat($pseudo, $msg, $time){
 		$comunication = new ComunicationRestBar();
-		$PARAMS = array($pseudo, $msg);
-		$PARAMS_TYPES = array (ComunicationRestBar::$TSTRING, ComunicationRestBar::$TSTRING);
+		$PARAMS = array($pseudo, $msg, $time);
+		$PARAMS_TYPES = array (ComunicationRestBar::$TSTRING, ComunicationRestBar::$TSTRING, ComunicationRestBar::$TSTRING);
 		$result = $comunication->query(self::ESCRIBIRCHAT,$PARAMS,$PARAMS_TYPES);
 		
 		return $result;
--- Guate/restbar/Presentacion/writeChat.php
+++ Guate/restbar/Presentacion/writeChat.php
@@ -4,7 +4,7 @@ $chat = new chat();
 $chatInfo = new chatInfo();
 $nickname=$_POST['nickname'];
 if ($nickname){
-	$chatInfo->pseudo=$nickname;
+	$chatInfo->setPseudo($nickname);
 	echo '"'.$nickname.'"';
 }else {
     $msg=trim(strip_tags($_POST['msg']));
@@ -19,5 +19,14 @@ if ($nickname){
 <?php
 class chatInfo{
   public $pseudo;
+   	public function __construct (){
+   		session_start ();
+		ini_set("session.gc_maxlifetime", "18000");	//5 horas
+		$this->pseudo=$_SESSION['pseudo'];
+   	}
+   	public function setPseudo($nick){
+   		$_SESSION['pseudo']=$nick;
+   		$this->pseudo=$nick;
+   	}
 }
 ?>
