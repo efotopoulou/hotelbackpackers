4e333796966192b04a591333c90b9aec6ddecc26
fwtopoulou <fwtopoulou@776907b2-8993-11dd-9899-c7ba65f7c4c7>
2008-10-17 19:57:53 +0000

    chat 2

--- trunk/Guate/restbar/Presentacion/readchat.php
+++ trunk/Guate/restbar/Presentacion/readchat.php
@@ -1,6 +1,21 @@
 <?php
 require ($_SERVER['DOCUMENT_ROOT'] . '/restbar/Dominio/class_chat.php');
 
+$chat = new chat();
+$nickname=$_POST['nickname'];
+$msg=$_POST['msg'];
+if ($nickname){
+	$_SESSION['pseudo']=$nickname;
+	echo '"'.$nickname.'"';
+}else if ($msg){
+    $msg=trim(strip_tags($_POST['msg']));
+    $messages=$chat->escribirChat($_SESSION['pseudo'], $msg);
+    $data=array('msg' => $msg, 'pseudo' => $_SESSION['pseudo']);
+    if(!empty($msg))
+        echo json_encode($data);
+    else
+        echo json_encode('');
+}else {
     if(empty($_SESSION['last_chat_message_id'])) $_SESSION['last_chat_message_id']=0;
     $chat = new chat();
     $messages=$chat->leerChat($_SESSION['pseudo']);
@@ -9,4 +24,10 @@ require ($_SERVER['DOCUMENT_ROOT'] . '/restbar/Dominio/class_chat.php');
         $_SESSION['last_chat_message_id']=$tab_messages[count($tab_messages)-1]->id_msg;
         echo json_encode($tab_messages);
     } else echo json_encode('');
+}
+?>
+<?php
+class chatInfo{
+  public $pseudo;
+}
 ?>
