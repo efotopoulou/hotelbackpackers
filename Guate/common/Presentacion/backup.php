9561e8c67b2d59892c750525a3f14ac0e9d3314e
willezumleben <willezumleben@776907b2-8993-11dd-9899-c7ba65f7c4c7>
2009-10-31 12:23:49 +0000

    Arreglado backup.

--- Guate/common/Presentacion/backup.php
+++ Guate/common/Presentacion/backup.php
@@ -1,15 +1,18 @@
 <?php
 // This code was created by phpMyBackupPro v.2.1 
 // http://www.phpMyBackupPro.net
+
 $_POST['db']=array("guate_bd", "recepcion_bd", "restbar_bd" );
 $_POST['tables']="on";
 $_POST['data']="on";
 $_POST['drop']="on";
 $_POST['zip']="gzip";
 $_POST['mysql_host']="-1";
-$period=(3600*24)/24;
+$period=(3600*24)*0;
 $security_key="";
 // This is the relative path to the phpMyBackupPro v.2.1 directory
 @chdir("../../common/phpMyBackupPro/");
+echo "hola2";
 @include("backup.php");
+echo "hola2";
 ?>
